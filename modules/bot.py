c5decee8d610165ddeeb1effe9a463202684de57
Fi <158217226+lightfi7@users.noreply.github.com>
2024-10-23 18:07:34 -0700

    Update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -310,7 +310,7 @@ class Bot:
             submit_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type=submit]')))
             submit_button.click()
 
-            show_notification('SMDA', 'Waiting 3 minutes for results...')
+            show_notification('SMDA', 'Waiting 3 minutes to get results. Never touch the scroll.')
             time.sleep(60*3) # Waiting 5 mins to get data
 
             self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
